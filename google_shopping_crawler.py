1e869adea264f45872d739f927a9a2227f0ce868
shikbupt <shi-kuan@qq.com>
2014-01-07 19:58:56 +0800

    fixed

--- google_shopping_crawler.py
+++ google_shopping_crawler.py
@@ -3,22 +3,27 @@ from download_pics import download_pics
 from analyse_web_pages import WebPageAnalysis
 from Queue import Queue
 from random import randint
-from time import sleep
+from time import sleep, ctime
 import threading
+import os
 
 
 def main():
+    print 'start:', ctime()
     search_class = ['shoes']
-    max_download_threads = 10
+    max_download_threads = 20
     root = 'z:\\'
     threads = []
     queue = Queue()
     for item in search_class:
+        root_path = ''.join((root, item))
+        if not os.path.exists(root_path):
+            os.mkdir(root_path)
         url = 'http://www.google.co.uk/search?q=%s&tbm=shop&ei=HAGYUujUEIfvkQWPTw&ved=0CAMQyBAoAQ&pshpl=1&pshplp=2&num=10' % item
         t = threading.Thread(target=WebPageAnalysis(url, queue))
         threads.append(t)
-        for i in xrange(1,max_download_threads):
-            t = MyThread(download_pics, (root+item+'\\', queue), 'download_pics')
+        for i in xrange(0,max_download_threads):
+            t = MyThread(download_pics, (root_path+'\\', queue), 'download_pics')
             threads.append(t)
     threads[0].start()
     for i in threads[1:]:
@@ -29,7 +34,8 @@ def main():
 
     while not queue.empty():
         pass
-    sleep(5)
+    sleep(10)
+    print 'end:', ctime()
 
 if __name__ == '__main__':
     main()
