2f8b4ce08b4a99cddd9fc0c885f30e4d79be9565
shikbupt <shi-kuan@qq.com>
2014-01-07 18:42:04 +0800

    google_shopping_crawler done

--- google_shopping_crawler.py
+++ google_shopping_crawler.py
@@ -6,45 +6,30 @@ from random import randint
 from time import sleep
 import threading
 
-def get_queue(queue, logfile): 
-    while True:
-        temp = queue.get()
-        for url in temp:
-            print >> logfile, url
-        print >> logfile, '*****************************************'
-        queue.task_done()
 
 def main():
     search_class = ['shoes']
+    max_download_threads = 10
+    root = 'z:\\'
     threads = []
     queue = Queue()
     for item in search_class:
         url = 'http://www.google.co.uk/search?q=%s&tbm=shop&ei=HAGYUujUEIfvkQWPTw&ved=0CAMQyBAoAQ&pshpl=1&pshplp=2&num=10' % item
-        #t = MyThread(WebPageAnalysis, (url, queue), 'WebPageAnalysis')
         t = threading.Thread(target=WebPageAnalysis(url, queue))
         threads.append(t)
+        for i in xrange(1,max_download_threads):
+            t = MyThread(download_pics, (root+item+'\\', queue), 'download_pics')
+            threads.append(t)
     threads[0].start()
-
-    logfile = open('z:\\log4.txt', 'w')
-    t = MyThread(get_queue, (queue, logfile), 'get_queue')
-    t.setDaemon(True)
-    threads.append(t)
-    threads[1].start()
+    for i in threads[1:]:
+        i.setDaemon(True)
+        i.start()
     
     threads[0].join()
-    #threads[1].join()
 
     while not queue.empty():
         pass
     sleep(5)
-
-    '''logfile = open('z:\\log3.txt', 'w')
-    while not queue.empty():
-        temp = queue.get()
-        for url in temp:
-            print >> logfile, url
-        print >> logfile, '*****************************************'
-    '''
 
 if __name__ == '__main__':
     main()
