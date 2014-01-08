c770387eae9956eb535cb67a9f0a5699afe5fb85
shikbupt <shi-kuan@qq.com>
2014-01-08 16:22:41 +0800

    add searching class and analysised pages

--- google_shopping_crawler.py
+++ google_shopping_crawler.py
@@ -10,8 +10,9 @@ import os
 
 def main():
     print 'start:', ctime()
-    search_class = ['shoes']
+    search_class = ['shirt']
     max_download_threads = 20
+    analysis_pages = 2 # each page has 100 pics
     root = 'z:\\'
     threads = []
     queue = Queue()
@@ -19,22 +20,28 @@ def main():
         root_path = ''.join((root, item))
         if not os.path.exists(root_path):
             os.mkdir(root_path)
-        url = 'http://www.google.co.uk/search?q=%s&tbm=shop&ei=HAGYUujUEIfvkQWPTw&ved=0CAMQyBAoAQ&pshpl=1&pshplp=2&num=10' % item
-        t = threading.Thread(target=WebPageAnalysis(url, queue))
-        threads.append(t)
-        for i in xrange(0,max_download_threads):
+        for i in range(0,analysis_pages):
+            url = 'http://www.google.co.uk/search?q=%s&tbm=shop&ei=HAGYUujUEIfvkQWPTw&ved=0CAMQyBAoAQ&pshpl=1&pshplp=2&num=100&start=%s' \
+                    % (item, i*100)
+            print url
+            t = threading.Thread(target=WebPageAnalysis(url, queue))
+            threads.append(t)
+        for i in range(0,max_download_threads):
             t = MyThread(download_pics, (root_path+'\\', queue), 'download_pics')
             threads.append(t)
-    threads[0].start()
-    for i in threads[1:]:
+
+    for i in range(0,analysis_pages): 
+        threads[i].start()
+    for i in threads[analysis_pages:]:
         i.setDaemon(True)
         i.start()
     
-    threads[0].join()
+    for i in range(0,analysis_pages): 
+        threads[i].join()
 
     while not queue.empty():
         pass
-    sleep(10)
+    sleep(20)
     print 'end:', ctime()
 
 if __name__ == '__main__':
